30f3ab29e02d4e164d319a45a1ff1b810f10d746
Ivan Solorzano <solorzanoivan608@gmail.com>
2025-05-15 21:08:39 -0500

    Agregamos nuestra activity de logout para generar el boton de acceso de nuestra activity_home.

--- app/src/main/java/com/example/kickup/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/kickup/Activities/HomeActivity.kt
@@ -1,17 +1,24 @@
 package com.example.kickup.Activities
 
 import android.os.Bundle
-import android.os.Handler
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kickup.R
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val boton = findViewById<Button>(R.id.btn_pre_logout)
+
+        boton.setOnClickListener {
+            val intent = Intent(this, LogoutActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/kickup/Activities/LogoutActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kickup.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kickup.R
+
+class LogoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logout)
+    }
+}
