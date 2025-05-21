7d295b9698672b06a1336175ec8d3cacae68552b
Ivan Solorzano <solorzanoivan608@gmail.com>
2025-05-20 23:08:23 -0500

    Desarrollamos todo el layout de nuestra pagina principal, aun faltan algunos detalles sin embargo se la visual es clara. Se tiene la idea del CRUD sin embargo aun no se tiene full funcionalidad. En nuestra activity establecemos la actividad de algunos botones para empezar a crear informes.

--- app/src/main/java/com/example/kickup/Activities/MajorActivity.kt
+++ app/src/main/java/com/example/kickup/Activities/MajorActivity.kt
@@ -1,15 +1,33 @@
 package com.example.kickup.Activities
 
 import android.os.Bundle
+
+import android.view.View
+import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kickup.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MajorActivity : AppCompatActivity() {
 
+    private lateinit var formularioContainer: ScrollView
+    private lateinit var btnCrear: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_major)
 
+        formularioContainer = findViewById(R.id.formularioContainer)
+        btnCrear = findViewById(R.id.btnCrear)
+
+        //boton crear
+        btnCrear.setOnClickListener {
+            if (formularioContainer.visibility == View.GONE) {
+                formularioContainer.visibility = View.VISIBLE
+            } else {
+                formularioContainer.visibility = View.GONE
+            }
+        }
     }
 }
+
