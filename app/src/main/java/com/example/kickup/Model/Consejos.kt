26a344b00f9ff11545ad334aa7b460bf3db82d34
Ivan Solorzano <solorzanoivan608@gmail.com>
2025-05-31 18:29:21 -0500

    Aca simplmente definimos la clase con nuestra lista.

---
+++ app/src/main/java/com/example/kickup/Model/Consejos.kt
@@ -0,0 +1,13 @@
+package com.example.kickup.Model
+
+data class Consejos(
+    val id: Int,
+    val user_id: Int,
+    val tittle: String,
+    val difficulty: String,
+    val position: String,
+    val goals: String,
+    val csj1: String,
+    val csj2: String,
+    val csj3: String
+)
