53ed061b95e540fdf87ef452749327c574beaf54
Ivan Solorzano <solorzanoivan608@gmail.com>
2025-05-31 18:18:05 -0500

    Aca simplemente correjimos sintaxis del codigo, y agregamos configuracion de los dos botones que tenemos de registrate y logout.

--- app/src/main/java/com/example/kickup/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/kickup/Activities/HomeActivity.kt
@@ -8,17 +8,22 @@ import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        //CONFIGURA EL BOTÓN PARA QUE ME LLEVE DE LOGIN (LogoutActivity)
         val boton = findViewById<Button>(R.id.btn_pre_logout)
-
         boton.setOnClickListener {
             val intent = Intent(this, LogoutActivity::class.java)
             startActivity(intent)
+        }
 
+        //CONFIGURA EL BOTÓN PARA QUE ME LLEVE DE REGISTRO (RegistrateActivity)
+        val botons = findViewById<Button>(R.id.btn_registrate)
+        botons.setOnClickListener {
+            val intent = Intent(this, RegistrateActivity::class.java)
+            startActivity(intent)
         }
     }
 }
